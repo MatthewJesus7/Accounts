ed4c79d5d7a8c7c785d9d86231fbde3aef29665f
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-10-20 20:01:01 -0300

    tailwind css funcionando com 100% de certeza

--- appdefaxina/app/index.jsx
+++ appdefaxina/app/index.jsx
@@ -8,12 +8,16 @@ NativeWindStyleSheet.setOutput({
   default: "native",
 });
 
+import MyComponent from '../components/MyComponent';
+
 const App = () => {
   return (
     <View className="flex-1 justify-center items-center bg-gray-100">
       <Text className="text-2xl font-bold">
         Hello, NativeWind!
       </Text>
+
+      <MyComponent></MyComponent>
 
     </View>
   );
--- appdefaxina/tailwind.config.js
+++ appdefaxina/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   content: [
     // "./App.{js,jsx,ts,tsx}",
     "./app/**/*.{js,jsx,ts,tsx}",
-    // "./components/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
     // "node_modules/nativewind/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
