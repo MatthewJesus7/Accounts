ee52e4185598378e0114dfa51d428cd9149e4301
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-10-21 16:39:00 -0300

    instalação do firebase

--- appdefaxina/app/index.jsx
+++ appdefaxina/app/index.jsx
@@ -12,19 +12,7 @@ import MyComponent from '../components/MyComponent';
 
 const App = () => {
   return (
-    <View className="flex-1 justify-center items-center bg-gray-100">
-      <View className="-mr-[12px]">
-        <View className="rounded-full -mb-[52px] ml-[11px] w-12 h-14 bg-red-500 rotate-45"></View>
-        <View className="rounded-full -mb-6 -ml-[23px] w-14 h-12 bg-red-500 rotate-45"></View>
-        <View className="w-10 h-10 bg-red-500 rotate-45"></View>
-      </View>
-      <Text className="text-2xl mt-5 bg-red-500 font-bold px-2 rounded-lg">
-        te amo Shakira
-      </Text>
-
-      <Text className="font-bold">
-        Amor da minha vida
-      </Text>
+    <View>
 
       {/* <MyComponent></MyComponent> */}
 
