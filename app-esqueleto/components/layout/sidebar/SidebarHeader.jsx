a04a036cd21bda62201b9118d200ce16ea09b577
Matheus Costa Jesus <mc5349063@gmail.com>
2025-01-17 23:03:37 -0300

    apagando tudo

--- app-esqueleto/components/layout/sidebar/SidebarHeader.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-
-export const SidebarHeader = ({ title }) => (
-  <View className="p-4 border-b border-gray-700">
-    <Text className="text-white text-xl font-bold">{title}</Text>
-  </View>
-);
--- app-esqueleto/components/layout/sidebar/SidebarMenuItem.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
-
-export const SidebarMenuItem = ({ title }) => (
-  <TouchableOpacity className="py-2 px-4 hover:bg-gray-700">
-    <Text className="text-white">{title}</Text>
-  </TouchableOpacity>
-);
